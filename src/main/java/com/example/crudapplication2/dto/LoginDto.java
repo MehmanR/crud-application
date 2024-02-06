47073f84303cb3ef542bc40855d15edca001b8b4
User <rustamzademehman96@gmail.com>
2024-02-06 19:23:39 +0400

    validation for user compilited

--- src/main/java/com/example/crudapplication2/dto/LoginDto.java
+++ src/main/java/com/example/crudapplication2/dto/LoginDto.java
@@ -1,11 +1,23 @@
 package com.example.crudapplication2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@JsonInclude
 public class LoginDto {
+
+    @NotBlank
+    @Size(min = 8, max = 255)
     private String password;
+
+    @NotBlank
+    @Email(message = "email duzgun daxil edin!")
+    @Size(min = 8, max = 255)
     private String email;
 }
--- src/main/java/com/example/crudapplication2/dto/UserDto.java
+++ src/main/java/com/example/crudapplication2/dto/UserDto.java
@@ -1,20 +1,40 @@
 package com.example.crudapplication2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @Builder
+@JsonInclude
 public class UserDto {
 
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String surname;
+
+    @NotBlank
+    @Email(message = "email duzgun daxil edin!")
+    @Size(min = 8, max = 255)
     private String email;
+
+    @NotBlank
+    @Size(min = 12, max = 14)
+    @Pattern(regexp = "[+][0-9]{12}", message = "Telefonu duzgun daxil edin")
     private String phoneNumber;
+
+    @NotBlank
+    @Past
     private LocalDate birthDate;
 
 
